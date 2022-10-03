b5d1e00229b0aca8c84c6e11e58ee287e8fccc40
repl.it user <replituser@example.com>
2022-10-03 22:24:47 +0200

    Add email_validator

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL, email_validator
 from flask_ckeditor import CKEditorField
 
 
